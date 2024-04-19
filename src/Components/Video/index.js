1eadefb31a0ab74f2304604e7ac68ba39d4a81c3
Rakesh Mali <rakeshmali@Rakeshs-MacBook-Pro.local>
2024-04-19 14:57:27 +0530

    Fix: (MainVideo) UI issue fixes for video

--- src/Components/Video/index.js
+++ src/Components/Video/index.js
@@ -6,7 +6,7 @@ import { MAIN_VIDEO } from "../../Constants";
 const Video = ({ onEdit, iframeUrl }) => {
   return (
     <Fragment>
-      {iframeUrl && (
+      {iframeUrl ? (
         <Box
           component={"iframe"}
           sx={{
@@ -15,6 +15,7 @@ const Video = ({ onEdit, iframeUrl }) => {
             right: 0,
             top: 0,
             bottom: 0,
+            zIndex: 1,
           }}
           id="edit-iframe"
           src={iframeUrl}
@@ -23,37 +24,41 @@ const Video = ({ onEdit, iframeUrl }) => {
           title="Edited Video"
           allowFullScreen
         />
-      )}
-
-      <Grid
-        container
-        justifyContent="center"
-        alignItems="center"
-        style={{ minHeight: "100vh" }}
-      >
-        <Grid item sx={{ textAlign: "center" }}>
-          <Box
-            sx={{
-              display: "flex",
-              mb: 2,
-              borderRadius: 1,
-              overflow: "hidden",
-            }}
-            boxShadow={1}
-          >
-            <iframe
-              src={MAIN_VIDEO}
-              width="560"
-              height="315"
-              title="Justin Trudeau & Barack Obama White House Rose Garden media conference, March 10, 2016"
-              frameBorder="0"
-            ></iframe>
-          </Box>
-          <Button onClick={onEdit} variant="contained" color="primary">
-            Edit Video
-          </Button>
+      ) : (
+        <Grid
+          container
+          justifyContent="center"
+          alignItems="center"
+          style={{ minHeight: "100vh" }}
+        >
+          <Grid item sx={{ textAlign: "center", p: 1 }}>
+            <Box
+              sx={{
+                display: "flex",
+                mb: 2,
+                borderRadius: 1,
+                overflow: "hidden",
+              }}
+              boxShadow={1}
+            >
+              <Box
+                component={"iframe"}
+                sx={{
+                  maxWidth: 1,
+                  border: 0,
+                }}
+                src={MAIN_VIDEO}
+                width="560px"
+                height="315px"
+                title="Justin Trudeau & Barack Obama White House Rose Garden media conference, March 10, 2016"
+              ></Box>
+            </Box>
+            <Button onClick={onEdit} variant="contained" color="primary">
+              Edit Video
+            </Button>
+          </Grid>
         </Grid>
-      </Grid>
+      )}
     </Fragment>
   );
 };
